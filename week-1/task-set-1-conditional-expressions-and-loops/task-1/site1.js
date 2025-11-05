f86950a0ad87b8b5d298d9599bddc9fb3e16c978
sandra-sanj <sandra.juursoo@metropolia.fi>
2025-11-05 18:04:22 +0200

    remove unnecessary files for branch

--- week-1/task-set-1-conditional-expressions-and-loops/task-1/site1.js
+++
@@ -1,5 +0,0 @@
-const celcius = prompt('Enter temperature in celcius:');
-const fahrenheit = (celcius * 9) / 5 + 32;
-
-document.getElementById('celcius').innerHTML = 'celcius: ' + celcius;
-document.getElementById('fahrenheit').innerHTML = 'fahrenheit: ' + fahrenheit;
