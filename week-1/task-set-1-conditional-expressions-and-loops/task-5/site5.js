8c5b48632ce237947596cf61253f7da880eb82e2
sandra-sanj <sandra.juursoo@metropolia.fi>
2025-11-06 17:28:35 +0200

    delete unneeded files

--- week-1/task-set-1-conditional-expressions-and-loops/task-5/site5.js
+++
@@ -1,8 +0,0 @@
-const naturalNumber = prompt('Enter positive number:');
-
-let sum = 0;
-for (let number = 1; number <= naturalNumber; number++) {
-  sum += number;
-}
-
-document.getElementById('sum').innerHTML = sum;
--- week-2/task-set-5-advanced-javascript-concepts-array-methods-and-higher-order-functions/variables.js
+++
@@ -1,8 +0,0 @@
-const baseUrl = 'https://media2.edu.metropolia.fi/restaurant';
-
-// which language restaurant data is fetched from API
-const lang = 'en';
-
-const highlightClass = 'highlight';
-
-export {baseUrl, lang, highlightClass};
