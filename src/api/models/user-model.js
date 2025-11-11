4818e91bd594ac7d62e03f3c4a7d9d0748993c07
sandra-sanj <sandra.juursoo@metropolia.fi>
2025-11-12 00:14:34 +0200

    add transaction to removeUser function

--- src/api/models/user-model.js
+++ src/api/models/user-model.js
@@ -49,15 +49,40 @@ const modifyUser = async (user, id) => {
 };
 
 const removeUser = async (id) => {
-  const [rows] = await promisePool.execute(
-    'DELETE FROM wsk_Users WHERE user_id = ?',
-    [id]
-  );
-  console.log('rows', rows);
-  if (rows.affectedRows === 0) {
-    return false;
+  // get a connection object from the pool
+  const connection = await promisePool.getConnection();
+
+  try {
+    await connection.beginTransaction();
+    await connection.execute('DELETE FROM wsk_cats WHERE owner = ?;', [id]);
+    const sql = connection.format('DELETE FROM wsk_users WHERE user_id = ?', [
+      id,
+    ]);
+
+    const [result] = await connection.execute(sql);
+
+    if (result.affectedRows === 0) {
+      return {
+        message: 'User not deleted',
+      };
+    }
+
+    // if no errors, commit the transaction (save changes)
+    await connection.commit();
+
+    return {
+      message: 'User deleted',
+    };
+  } catch (error) {
+    // if error, rollback transaction (undo changes)
+    await connection.rollback();
+    console.error('error', error.message);
+    return {
+      message: error.message,
+    };
+  } finally {
+    connection.release();
   }
-  return {message: 'success'};
 };
 
 const getUserByUsername = async (username) => {
